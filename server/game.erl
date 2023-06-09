3dc3d6b2956830f4ec949a2e00ced063d32f2d75
Bruno Silva <brunocunhasilva2001@gmail.com>
2023-06-09 02:11:48 +0100

    change state from list to tuple, added input()

--- server/game.erl
+++ server/game.erl
@@ -6,58 +6,46 @@ match(ServerPid, Players) ->
     ServerPid ! {start, self()},    
     [FromPid ! {start, self()} || {_User, FromPid} <- Players],
     State = initMatch(Players),
-    matchTimer(State, []).
+    matchTimer(State).
 
 initMatch(Players) ->
+    % State = [ [Username1, x1, y1, color1,  radius, direction1, angularVel1, linearAcel1, score1, FromPid1 ],
+    %           [Username2, x2, y2, color2, radius, direction2, angularVel2, linearAcel2, score2, FromPid2 ],
+    %           [[Color, x, y], [Color, x, y]]]
     [{User1, FromPid1},{User2, FromPid2}] = Players,
-    P1 = [User1, "black", 100, 100, 5.5, 0.23, 2.13, 0, FromPid1],
-    P2 = [User2, "white", 700, 700, 2.36, 0.23, 2.13, 0, FromPid2],
-    State = [P1, P2],
+    P1 = {User1, 100, 100, "black", 20, 5.5, 2.13, 0.23, 0, FromPid1},
+    P2 = {User2, 700, 700, "white", 20, 2.36, 2.13, 0.23, 0, FromPid2},
+    State = {P1, P2, []},
     State.
 
-matchTimer(State, Food) ->
+matchTimer(State) ->
     % io:format("Match Timer ~n"),
     Self = self(),
     spawn(fun() -> receive after 1 -> Self ! timeout end end),
 
-    {NewState, TmpFood} = handleMatch(State, Food),
+    NewState = handleMatch(State),
     % new food = genFood
     % NewState is a list so already parsed? ///  PlayerInfo = parseMatch(maps:to_list(NewState), []),
     % [FromPid ! Info || {asdas d} <- maps:to_list(NewState)],
-    loop(NewState, TmpFood). % sub by NewFood
+    loop(NewState). % sub by NewFood
 
-handleMatch(State, Food) ->
+handleMatch(State) ->
     FinalState = findCollisions(State),
-    {FinalState, Food}.
+    FinalState.
 
 findCollisions(State) -> 
-    % State = [ [User1, "black", 100, 100, 5.5, 0.23, 2.13, 0, FromPid1],  [User2, "white", 700, 700, 2.36, 0.23, 2.13, 0, FromPid2] ]
-    {XP1, YP1} = { lists:nth(3, lists:nth(1, State)), lists:nth(4, lists:nth(1, State)) }, 
-    {XP2, YP2} = { lists:nth(3, lists:nth(2, State)), lists:nth(4, lists:nth(2, State)) },
-    Raio = 50,
-    Dist = math:sqrt((XP2-XP1)*(XP2-XP1) + (YP2-YP1)*(YP2-YP1)), 
-    if Dist < 2 * Raio ->  % mf just crashed
-           ok;
-           %io:format("Crash ~n");
-           % figure out who rear ended who 
-       true ->
-           ok
-           %io:format("No crash ~n")
-    end,
-    %FinalState leva nova pos de quem foi atropelado
-    %FinalState.
     State.
 
-loop(State, Food) ->
+loop(State) ->
     receive
         timeout ->
-            matchTimer(State, Food);
+            matchTimer(State);
         {leave, User, FromPid} ->
             FromPid ! leave;
             % removePlayer
         {move, {Username, Direction} ,_} ->
-            NewState = input(),
-            loop(NewState, Food)
+            NewState = input(State, Username, Direction),
+            loop(NewState)
     end.
 
 clientMatchLoop(Sock, LobbyPid, Username) ->
@@ -72,9 +60,52 @@ clientMatchLoop(Sock, LobbyPid, Username) ->
             end,
             clientMatchLoop(Sock, LobbyPid, Username)
     end.
- 
-input() ->
-    ok.
+
+input(State, Username, Direction) ->
+    {{User1, _, _, _, _, _, _, _, _, _}, {User2, _, _, _, _, _, _, _, _, _}, _} = State,
+    {P1, P2, Food} = State,
+    case Username of
+        User1 ->
+            {User, X, Y, Color, Radius, Dir, AngVel, LinAcel, Score, FromPid} = P1;
+        User2 ->
+            {User, X, Y, Color, Radius, Dir, AngVel, LinAcel, Score, FromPid} = P2
+    end,
+    case Direction of
+        "left" ->
+            NewDir = Dir + AngVel, % Dir + (AngVel / tickrate)? , AVP = m/s  , tickrate = tick/s
+            NewX = X,
+            NewY = Y;
+        "right" ->
+            NewDir = Dir - AngVel,
+            NewX = X,
+            NewY = Y;
+        "up" ->
+            NewDir = Dir,
+            NewX = X + (LinAcel * (math:cos(NewDir) - x)),
+            NewY = Y + (LinAcel * (math:sen(NewDir) - y));
+        "up#left" ->
+            NewDir = Dir + AngVel,
+            NewX = X + (LinAcel * (math:cos(NewDir) - x)),
+            NewY = Y + (LinAcel * (math:sen(NewDir) - y));
+        "up#right" ->    
+            NewDir = Dir - AngVel,
+            NewX = X + (LinAcel * (math:cos(NewDir) - x)),
+            NewY = Y + (LinAcel * (math:sen(NewDir) - y));
+        _ ->
+            io:format("Debug input error ~p~n", [Dir]),
+            NewDir = Dir,
+            NewX = X,
+            NewY = Y
+    end,
+    case Username of
+        User1 ->
+            NewP1 = {User, NewX, NewY, Color, Radius, NewDir, AngVel, LinAcel, Score, FromPid},
+            NewState = {NewP1, P2, Food};
+        User2 ->
+            NewP2 = {User, NewX, NewY, Color, Radius, NewDir, AngVel, LinAcel, Score, FromPid},
+            NewState = {P1, NewP2, Food}
+    end,
+    NewState.
 
 % ProcessingPlayer = {username, colr, x, y, direction}
 % ServerPlayer = {username, color, x, y, direction, linearAcel, angularVeloc, score}
